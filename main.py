6867595b3287b3c7649ed32b2103c04819e9b1f0
Sigintt <133558809+Sigintt@users.noreply.github.com>
2023-05-15 17:11:31 +0300

    Update main.py
    Fix Comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import requests
 
-# Define a class to check user input
+# Define a class to check user input.
 class CheckInputNumber():
     def __init__(self):
-        # Prompt the user to enter a number between 1-100
+        # Prompt the user to enter a number between 1-100.
         print('Welcome to Name Generator !')
         userInput = int(input('Enter a Number between 1-100 : '))
         self.userInput = userInput
 
-    # Define a method to validate user input
+    # Define a method to validate user input.
     def InputValidation(self):
-        # Check if user input is between 1-100
+        # Check if user input is between 1-100.
         if self.userInput < 1 or self.userInput > 100:
             print(f'{self.userInput} not between 1-100')
             return False
@@ -23,14 +23,14 @@ class CheckInputNumber():
 class PrintName(CheckInputNumber):
     # Define a method to generate random names
     def generate_names(self):
-        # Call the InputValidation method to validate user input
+        # Call the InputValidation method to validate user input.
         if not self.InputValidation():
             return
 
-        # Generate the specified number of names using the Random User API
+        # Generate the specified number of names using the Random User API.
         counter = 0
         while counter < self.userInput:
-            # Make a request to the Random User API using the requests library
+            # Make a request to the Random User API using the requests library.
             res = requests.get('https://randomuser.me/api/')
             # Convert the JSON response into a Python dictionary
             nameJson = res.json()
@@ -42,6 +42,6 @@ class PrintName(CheckInputNumber):
             counter += 1
 
 
-# Create an instance of the PrintName class and call the generate_names method
+# Create an instance of the PrintName class and call the generate_names method.
 printName = PrintName()
 printName.generate_names()
